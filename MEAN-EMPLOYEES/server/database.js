3fc90f3b37a3c0ece9f0e0fb6059c7cd823947d5
roberto herrera <robertherrerajorquera@gmail.com>
2018-07-19 00:23:28 -0400

    robertoherrera
    aqui se realizo un app web a base de MEAN stack la cual posee un crud funcional, exepto con respecto a la actualización de datos por el momento. Aun esta en desarrollo

--- MEAN-EMPLOYEES/server/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose');
-
-const URI='mongodb://localhost/mean-crud';
-
-mongoose.connect(URI)
-    .then(db => console.log('db is connected'))//.then escucha los datos de la bd, ha pasado la prueba
-    .catch(err => console.log(err));//no ha pasado la prueba
-
-module.exports = mongoose;//exportacion, con esto la cstante mongoose le devuelve la coneccion
